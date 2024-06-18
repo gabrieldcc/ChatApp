8201fb40447a54c5f4e29ba4fad903029ba8fa2d
Gabriel Castro <gabrieldcc@gmail.com>
2024-06-18 13:03:35 -0300

    getting started

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp
+//
+//  Created by Gabriel de Castro Chaves on 18/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
