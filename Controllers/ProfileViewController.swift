c6ef35def4c88b251cece2f4290ae24faa23260d
Gabriel Castro <gabrieldcc@gmail.com>
2024-06-20 15:12:22 -0300

    feature: tabbar and logout

--- Controllers/ProfileViewController.swift
+++ Controllers/ProfileViewController.swift
@@ -6,24 +6,53 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ProfileViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let data = ["Log out"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        data.count
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.textAlignment = .center
+        cell.textLabel?.textColor = .red
+        return cell
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        do {
+            try FirebaseAuth.Auth.auth().signOut()
+            
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+        } catch {
+            print("Failed to Log out")
+        }
+        
+    }
+    
+    
 }
