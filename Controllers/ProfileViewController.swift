6a9253f2271a5ad12dea876fa6a62a0e2a1c47fa
Gabriel Castro <gabrieldcc@gmail.com>
2024-06-30 14:07:20 -0300

    feature: set profile pic

--- Controllers/ProfileViewController.swift
+++ Controllers/ProfileViewController.swift
@@ -50,8 +50,30 @@ class ProfileViewController: UIViewController {
         imageView.layer.borderWidth = 3
         imageView.layer.masksToBounds = true
         headerView.addSubview(imageView)
+        
+        StorageManager.shared.downloadUrl(for: path, completion: { result in
+            switch result {
+            case .success(let url):
+                self.downloadImage(imageView: imageView, url: url)
+            case .failure(let error):
+                print("Failed to get download url: \(error)")
+            }
+        })
 
         return headerView
+    }
+    
+    func downloadImage(imageView: UIImageView, url: URL) {
+        URLSession.shared.dataTask(with: url, completionHandler: { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            DispatchQueue.main.async {
+                let image = UIImage(data: data)
+                imageView.image = image
+            }
+        }).resume()
     }
 }
 
