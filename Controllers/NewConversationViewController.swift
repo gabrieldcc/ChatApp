46fec182ba17972ad00549aaabe49848e7b629c3
Gabriel Castro <gabrieldcc@gmail.com>
2024-06-30 22:00:55 -0300

    bugfix: search users action

--- Controllers/NewConversationViewController.swift
+++ Controllers/NewConversationViewController.swift
@@ -10,11 +10,11 @@ import JGProgressHUD
 
 class NewConversationViewController: UIViewController {
     
-    private let spinner = JGProgressHUD(style: .dark)
-    
+    var results: [[String: String]] = []
     private var users: [[String: String]] = []
-    
     private var hasFetched: Bool = false
+    
+    private let spinner = JGProgressHUD(style: .dark)
     
     private let searchBar: UISearchBar = {
         let component = UISearchBar()
@@ -29,9 +29,9 @@ class NewConversationViewController: UIViewController {
         return component
     }()
     
-    private let noConversationsLabel: UILabel = {
+    private let noResultsLabel: UILabel = {
        let component = UILabel()
-        component.text = "No conversations!"
+        component.text = "No results!"
         component.textAlignment = .center
         component.textColor = .gray
         component.font = .systemFont(ofSize: 21, weight: .medium)
@@ -41,6 +41,12 @@ class NewConversationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(noResultsLabel)
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
         searchBar.delegate = self
         view.backgroundColor = .white
         navigationController?.navigationBar.topItem?.titleView = searchBar
@@ -56,11 +62,12 @@ class NewConversationViewController: UIViewController {
 
 extension NewConversationViewController: UISearchBarDelegate {
     
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         guard let text = searchBar.text, !text.replacingOccurrences(of: " ", with: "").isEmpty else {
             return
         }
         
+        results.removeAll()
         spinner.show(in: view)
         
         self.searchUsers(query: text)
@@ -71,17 +78,67 @@ extension NewConversationViewController: UISearchBarDelegate {
         //check if array has firebase results
         if hasFetched {
             //if it does: filter
+          filterUsers(with: query)
+        } else {
+            //if not, fetch then filter
             DatabaseManager.shared.getAllUsers(completion: { [weak self] result in
                 switch result {
                 case .success(let usersCollection):
+                    self?.hasFetched = true
                     self?.users = usersCollection
+                    self?.filterUsers(with: query)
                 case .failure(let error):
                     print("Failed to get users: \(error)")
                 }
             })
-        } else {
-            //if not, fetch then filter
         }
         //update the UI: either show or not results label
+    }
+    
+    func filterUsers(with term: String) {
+        guard hasFetched else {
+            return
+        }
+        
+        self.spinner.dismiss()
+        
+        var results: [[String: String]] = self.users.filter({
+            guard let name = $0["name"]?.lowercased() else {
+                return false
+            }
+            return name.hasPrefix(term.lowercased())
+        })
+        self.results = results
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        if results.isEmpty {
+            self.noResultsLabel.isHidden = false
+            self.tableView.isHidden = true
+        } else {
+            self.noResultsLabel.isHidden = true
+            self.tableView.isHidden = false
+            self.tableView.reloadData()
+        }
+    }
+}
+
+extension NewConversationViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = results[indexPath.row]["name"]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return results.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        //start conversation
     }
 }
